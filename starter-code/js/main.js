90455d89b15e13cdb61472e1cc46743f9e45ed2c
Joao Zsigmond <jzsiggy@gmail.com>
2019-09-26 09:37:59 -0300

    done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -25,9 +25,50 @@ const cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
-const memoryGame = new MemoryGame(cards);
+let memoryGame = new MemoryGame(cards);
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+let compute = (clickCount) => {
+  if (clickCount % 2 == 0 && clickCount != 0) {
+
+    let cardsFacingUp = document.querySelectorAll('.is-up');
+    console.log(cardsFacingUp);
+    let firstCard = cardsFacingUp[0];
+    let secondCard = cardsFacingUp[1];
+
+    if (memoryGame.checkIfPair(firstCard.getAttribute('data-card-name'), secondCard.getAttribute('data-card-name'))) {
+      firstCard.classList.add('blocked');
+      secondCard.classList.add('blocked');
+    } 
+    else {
+      setTimeout(() => {
+        firstCard.children[1].classList.toggle('back');
+        firstCard.children[0].classList.toggle('back');
+        firstCard.classList.remove('is-up');
+
+        secondCard.children[1].classList.toggle('back');
+        secondCard.children[0].classList.toggle('back');
+        secondCard.classList.remove('is-up');
+      }, 1000);
+    }
+
+
+    let pairsGuessed = document.querySelector('#pairs_guessed');
+    let pairsClicked = document.querySelector('#pairs_clicked');
+
+    if (pairsGuessed) {
+      pairsGuessed.innerText = memoryGame.pairsGuessed;
+      // console.log(pairsGuessed);
+    }
+
+    if (pairsClicked) {
+      pairsClicked.innerText = memoryGame.pairsClicked;
+      // console.log(pairsClicked);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -37,15 +78,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory_board').innerHTML = html;
+  let board = document.querySelector('#memory_board');
+  let clicks = 0;
+
+
+  if (board) {
+    board.innerHTML = html;
+  };
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked: ', card);
+    card.onclick = function(e) {
+      clicks++;
+      console.log(clicks);
+
+      e.target.parentElement.classList.add('is-up');
+      e.target.parentElement.querySelector('.front').classList.toggle("back");
+      e.target.parentElement.querySelector('.back').classList.toggle("back");
+
+      compute(clicks);
     };
   });
+  
 });
 
 
